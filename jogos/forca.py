83a811f157d2f85e7a255dc20d6da6d5c4b79f2e
Yasminpiornedo <159542694+Yasminpiornedo@users.noreply.github.com>
2025-05-06 09:04:16 -0300

    Add files via upload

---
+++ jogos/forca.py
@@ -0,0 +1,24 @@
+def jogar():
+    print("*********************************")
+    print("***Bem vindo ao jogo da Forca!***")
+    print("*********************************")
+ 
+    palavra_secreta = "banana" 
+    enforcou = False
+    acertou = False
+
+    while (not enforcou and  not acertou):
+        chute = input("Qual é a letra?")
+         
+        index = 0
+        for letra in palavra_secreta:
+            if(chute==letra):
+                print(f'encontrei a letra { letra} na posição {index}')
+                index = index + 1
+
+        print("jogando...")
+
+    print("Fim do jogo")
+
+if(__name__ == "__main__"):
+    jogar()
